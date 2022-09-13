eb8fe280d6ca98f07a550aebd47b4c74ad5c5806
Binita Gupta <binitagupta24@gmail.com>
2022-09-13 17:18:37 -0400

    Fix bugs with newline breaking and lemmatize words

--- scraper.py
+++ scraper.py
@@ -1,5 +1,15 @@
 import requests
+import sys
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
 from bs4 import BeautifulSoup
+
+tokenizer = RegexpTokenizer(r'\w+')
+wordnet_lemmatizer= WordNetLemmatizer()
+stop_words = set(stopwords.words('english'))
 
 # Iterate through list of URLs
 with open("songs_list.txt","r") as f:
@@ -9,6 +19,21 @@ with open("songs_list.txt","r") as f:
 
         soup = BeautifulSoup(page.content, "html.parser")
 
+        # Scrape only from the divs that contain lyrics.
         results = soup.find_all(attrs={"data-lyrics-container": "true"})
+
+        textString = ""
+
         for result in results:
-            print(result.text)
+            # Replace <br> tags with a space to avoid word concatenation bug.
+            result = result.get_text(separator=" ").strip()
+            word_tokens = tokenizer.tokenize(result)
+
+            # Lemmatize word tokens.
+            for w in word_tokens:
+                if w not in stop_words:
+                    lemmatized = wordnet_lemmatizer.lemmatize(w)
+                    textString += str(lemmatized).strip('[]')
+                    textString += " "
+        
+        print(textString)
